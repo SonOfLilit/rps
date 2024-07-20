cd330fa6acea2792a80368a48fcbe918ca8f7c83
Aur Saraf <aur@sarafconsulting.com>
2024-07-21 01:15:22 +0300

    tournament

---
+++ tournament.py
@@ -0,0 +1,28 @@
+import random
+import rps
+import json
+
+PLAYERS = {
+    'aur': 'S!B|P?>!B',
+    'ehud': '~!B|P@<!B',
+    'hed': '~!B|P?!B',
+    'amir': '~!~!B|S@P?-P@+>!B',
+}
+
+def tournament():
+    random.seed(42)
+    scores = {p: 0 for p in PLAYERS}
+    for a in PLAYERS:
+        for b in PLAYERS:
+            if a != b:
+                prog_a = {k: v for k, v in zip("ABCD", PLAYERS[a].split("|"))}
+                prog_b = {k: v for k, v in zip("ABCD", PLAYERS[b].split("|"))}
+                score, match_log, _full_log = rps.run_game(100, prog_a, prog_b, random.randint(0, 1<<64))
+                scores[a] += score
+                scores[b] -= score
+                print(a, b)
+                print(match_log)
+    print(json.dumps(scores, indent=2))
+
+if __name__ == "__main__":
+    tournament()
